e0a817e1bf33f35c809fdea53259be9e714bd14b
grantshandy <granthandy@proton.me>
2023-11-04 15:24:08 -0600

    fix naersk nativeBuildInputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  nixConfig.bash-prompt = "\[nix-develop\]$ ";
+  nixConfig.bash-prompt = "[nix-develop]$ ";
 
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
@@ -12,12 +12,16 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy tailwindcss ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+      in {
+        defaultPackage = naersk-lib.buildPackage {
+          nativeBuildInputs = with pkgs; [ tailwindcss ];
+          root = ./.;
         };
+        devShell = with pkgs;
+          mkShell {
+            buildInputs =
+              [ cargo rustc rustfmt pre-commit rustPackages.clippy tailwindcss ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
       });
 }
